ad8dd79d81188be408b3822c9aac17635c6e3f7e
saba <mahdaneparsa@gmail.com>
2025-01-20 09:55:55 +0330

    login and save token in cookie

---
+++ utils/httpReq.js
@@ -0,0 +1,18 @@
+const BASE_URL = "https://fakestoreapi.com";
+const postData = async (path, data) => {
+  try {
+    const response = await fetch(`${BASE_URL}/${path}`, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    alert("An error occured !");
+  }
+};
+
+export { postData };
